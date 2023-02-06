a623f18a77e11caf9462c9db9130b5c0d275f810
Ivan Sytnik <ivan.sytnik02@gmail.com>
2023-02-06 06:23:26 +0200

    delete component

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -5,77 +5,7 @@ import Filter from '../Filter/Filter'
 import RenderContacts from '../RenderContacts/RenderContacts'
 import { useDispatch, useSelector } from 'react-redux';
 import { nameAction, numberAction, addContactAction, deleteContactAction, filterContactAction } from "redux/contacts/contacts.slice";
-// class Contacts extends Component {
-//     state = {
-//      contacts: [],
-//        filter: ''
-//     }
 
-
-    
-   
-//     addContacts = data => {
-//      if(this.state.contacts.find(contact => contact.name === data.name))
-//      {
-//        alert(`${data.name} is already in contacts`)
-//      }
-//       else {
-//        const contact = {
-//          id: nanoid(),
-//          name: data.name,
-//          number: data.number
-//        }
-//       this.setState(update => ({
-//        contacts: [contact, ...update.contacts]
-//       }))
-//       console.log(this.state)
-//      }
-//      };
-
-//     contactsDelete = contactsID => {
-       
-//        this.setState(update => ({
-//         contacts: update.contacts.filter(contact => contact.id !== contactsID)
-//        }))
-       
-//      }
-
-//     changeFilter = e => {
-//      this.setState({filter: e.currentTarget.value})
-//     }
-    
-//     componentDidUpdate(prevProps, prevState) {
-//       if(this.state.contacts !== prevState.contacts) {
-//         localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-//         console.log("Saved localStorage")
-//       }
-//     }
-
-//     componentDidMount() {
-//       const local = localStorage.getItem('contacts');
-//       const contacts = JSON.parse(local)
-
-//       if(contacts) {
-//         this.setState({
-//           contacts: contacts
-//         })
-//       }
-     
-//     }
-
-//     render() {
-//      const normFilter = this.state.filter.toLowerCase();
-//      const filterContacts = this.state.contacts.filter(contact => 
-//        contact.name.toLowerCase().includes(normFilter))
-//      return(
-//        <>
-//          <SubmitContacts onSubmit={this.addContacts}></SubmitContacts>
-//          <Filter value={this.state.filter} onChange={this.changeFilter}></Filter>
-//          <RenderContacts items={filterContacts} contactsDelete={this.contactsDelete}></RenderContacts>
-//          </>
-//      )
-//     }
-//    }
 
 
 export default function Contacts() {
